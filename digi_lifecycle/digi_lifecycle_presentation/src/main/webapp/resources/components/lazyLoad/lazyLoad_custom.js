81f30f1465196e63fbda9e57f3cd46e7dd7de023
Lu <6161098+cafefish@users.noreply.github.com>
2011-08-12 13:31:32 +0000

    thumbs with lazyload effect

---
+++ digi_lifecycle/digi_lifecycle_presentation/src/main/webapp/resources/components/lazyLoad/lazyLoad_custom.js
@@ -0,0 +1,10 @@
+		$(function(){
+
+			$('#container').find("img").lazyload({
+					//placeholder: "http://www.appelsiini.net/projects/lazyload/img/grey.gif",
+					container: $("#container"),
+					//event: "click",
+					effect: "fadeIn"
+					});
+
+			});
