cd110d092ffef1a057f8da05451dabee7d3020d7
Friederike Kleinfercher <1484773+kleinfe@users.noreply.github.com>
2012-06-18 11:58:28 +0000

    new class for new md field

---
+++ digi_lifecycle/digi_lifecycle_logic/src/main/java/de/mpg/mpdl/dlc/vo/mods/ModsRelatedItem.java
@@ -0,0 +1,50 @@
+package de.mpg.mpdl.dlc.vo.mods;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlValue;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class ModsRelatedItem {
+	
+	
+	@XmlAttribute(name = "type")
+	private String type;
+	
+	@XmlAttribute(name = "name")
+	private String name;
+	
+	@XmlValue
+	private String value;
+
+	public String getValue() {
+		return value;
+	}
+
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
