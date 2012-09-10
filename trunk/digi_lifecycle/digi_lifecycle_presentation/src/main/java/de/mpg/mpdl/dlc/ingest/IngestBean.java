628c1d07bf47c8bc3e333df3d43c06f4eb377327
haarlaender <haarlaender@28ceb770-7456-11e2-abc6-2dfc910b5e1a>
2012-09-10 08:16:58 +0000

    Drag&Drop fix & sorting of ingested images

--- trunk/digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/dlc/ingest/IngestBean.java
+++ trunk/digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/dlc/ingest/IngestBean.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -111,6 +113,8 @@ public class IngestBean{
 	private Volume volume;
 	
 	private List<String> pagesOfVolume = new ArrayList<String>();
+	
+	private String sortImagesAlgorithm;
 	
 	 
 	@URLAction(onPostback=false)
@@ -521,14 +525,17 @@ public class IngestBean{
 	  
 	public void processDrop(DropEvent e) {
 		FileItem draggedFile = (FileItem)e.getDragValue();
-		FileItem droppedFile = (FileItem)e.getDropValue();
+		//FileItem droppedFile = (FileItem)e.getDropValue();
 		int draggedFileIndex = getImageFiles().indexOf(draggedFile);
-		int droppedFileIndex = getImageFiles().indexOf(droppedFile);
+		//int droppedFileIndex = getImageFiles().indexOf(droppedFile);
+		int droppedFileIndex = (Integer)e.getDropValue();
+		
+		
 		
 		if(draggedFileIndex < droppedFileIndex)
 		{
 			getImageFiles().remove(draggedFileIndex);
-			getImageFiles().add(droppedFileIndex, (DiskFileItem) draggedFile);
+			getImageFiles().add(droppedFileIndex-1, (DiskFileItem) draggedFile);
 		}
 		else
 		{
@@ -941,7 +948,173 @@ public class IngestBean{
 	
 	
 
+	public String sortImages()
+	{
+		if(sortImagesAlgorithm.equals("alphanumeric"))
+		{
+			Collections.sort(imageFiles, new AlphanumericDiskFileComparator());
+		}
+		else if (sortImagesAlgorithm.equals("intelligent"))
+		{
+			Collections.sort(imageFiles, new AlphanumComparator());
+		}
+		
+		return "";
+	}
 
+
+	public String getSortImagesAlgorithm() {
+		return sortImagesAlgorithm;
+	}
+
+
+	public void setSortImagesAlgorithm(String sortImagesAlgorithm) {
+		this.sortImagesAlgorithm = sortImagesAlgorithm;
+	}
+	
+	
+	public class AlphanumericDiskFileComparator implements Comparator<DiskFileItem>
+	{
+
+		@Override
+		public int compare(DiskFileItem o1, DiskFileItem o2) {
+			return o1.getName().compareTo(o2.getName());
+		}
+		
+	}
+	
+	/*
+	 * The Alphanum Algorithm is an improved sorting algorithm for strings
+	 * containing numbers.  Instead of sorting numbers in ASCII order like
+	 * a standard sort, this algorithm sorts numbers in numeric order.
+	 *
+	 * The Alphanum Algorithm is discussed at http://www.DaveKoelle.com
+	 *
+	 *
+	 * This library is free software; you can redistribute it and/or
+	 * modify it under the terms of the GNU Lesser General Public
+	 * License as published by the Free Software Foundation; either
+	 * version 2.1 of the License, or any later version.
+	 *
+	 * This library is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	 * Lesser General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU Lesser General Public
+	 * License along with this library; if not, write to the Free Software
+	 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+	 *
+	 */
+
+	
+
+	/**
+	 * This is an updated version with enhancements made by Daniel Migowski,
+	 * Andre Bogus, and David Koelle
+	 *
+	 * To convert to use Templates (Java 1.5+):
+	 *   - Change "implements Comparator" to "implements Comparator<String>"
+	 *   - Change "compare(Object o1, Object o2)" to "compare(String s1, String s2)"
+	 *   - Remove the type checking and casting in compare().
+	 *
+	 * To use this class:
+	 *   Use the static "sort" method from the java.util.Collections class:
+	 *   Collections.sort(your list, new AlphanumComparator());
+	 */
+	public class AlphanumComparator implements Comparator<DiskFileItem>
+	{
+	    private final boolean isDigit(char ch)
+	    {
+	        return ch >= 48 && ch <= 57;
+	    }
+
+	    /** Length of string is passed in for improved efficiency (only need to calculate it once) **/
+	    private final String getChunk(String s, int slength, int marker)
+	    {
+	        StringBuilder chunk = new StringBuilder();
+	        char c = s.charAt(marker);
+	        chunk.append(c);
+	        marker++;
+	        if (isDigit(c))
+	        {
+	            while (marker < slength)
+	            {
+	                c = s.charAt(marker);
+	                if (!isDigit(c))
+	                    break;
+	                chunk.append(c);
+	                marker++;
+	            }
+	        } else
+	        {
+	            while (marker < slength)
+	            {
+	                c = s.charAt(marker);
+	                if (isDigit(c))
+	                    break;
+	                chunk.append(c);
+	                marker++;
+	            }
+	        }
+	        return chunk.toString();
+	    }
+
+	    public int compare(DiskFileItem o1, DiskFileItem o2)
+	    {
+	    	/*
+	        if (!(o1 instanceof String) || !(o2 instanceof String))
+	        {
+	            return 0;
+	        }
+	        */
+	        String s1 = o1.getName();
+	        String s2 = o2.getName();
+
+	        int thisMarker = 0;
+	        int thatMarker = 0;
+	        int s1Length = s1.length();
+	        int s2Length = s2.length();
+
+	        while (thisMarker < s1Length && thatMarker < s2Length)
+	        {
+	            String thisChunk = getChunk(s1, s1Length, thisMarker);
+	            thisMarker += thisChunk.length();
+
+	            String thatChunk = getChunk(s2, s2Length, thatMarker);
+	            thatMarker += thatChunk.length();
+
+	            // If both chunks contain numeric characters, sort them numerically
+	            int result = 0;
+	            if (isDigit(thisChunk.charAt(0)) && isDigit(thatChunk.charAt(0)))
+	            {
+	                // Simple chunk comparison by length.
+	                int thisChunkLength = thisChunk.length();
+	                result = thisChunkLength - thatChunk.length();
+	                // If equal, the first different number counts
+	                if (result == 0)
+	                {
+	                    for (int i = 0; i < thisChunkLength; i++)
+	                    {
+	                        result = thisChunk.charAt(i) - thatChunk.charAt(i);
+	                        if (result != 0)
+	                        {
+	                            return result;
+	                        }
+	                    }
+	                }
+	            } else
+	            {
+	                result = thisChunk.compareTo(thatChunk);
+	            }
+
+	            if (result != 0)
+	                return result;
+	        }
+
+	        return s1Length - s2Length;
+	    }
+	}
 
 
 
