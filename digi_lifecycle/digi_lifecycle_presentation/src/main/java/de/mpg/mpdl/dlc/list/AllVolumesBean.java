e689cf3190294871cefd3f34e413f7470fde56e8
haarlaender <5524896+haarli@users.noreply.github.com>
2012-10-29 09:28:17 +0000

    some small fixes

--- digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/dlc/list/AllVolumesBean.java
+++ digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/dlc/list/AllVolumesBean.java
@@ -139,9 +139,10 @@ public class AllVolumesBean extends SortableVolumePaginatorBean {
 		scMenuList.add(new SelectItem(CombinedSortCriterion.AUTHOR_TITLE_ASC.name(), InternationalizationHelper.getLabel("sort_criterion_author")));
 		scMenuList.add(new SelectItem(CombinedSortCriterion.TITLE_YEAR_ASC.name(), InternationalizationHelper.getLabel("sort_criterion_title")));
 		scMenuList.add(new SelectItem(CombinedSortCriterion.YEAR_DESC, InternationalizationHelper.getLabel("sort_criterion_year")));
-		scMenuList.add(new SelectItem(CombinedSortCriterion.NEWEST_DESC.name(), InternationalizationHelper.getLabel("sort_criterion_newest")));
+		
 		if("my".equals(colId))
 		{
+			scMenuList.add(new SelectItem(CombinedSortCriterion.NEWEST_DESC.name(), InternationalizationHelper.getLabel("sort_criterion_newest")));
 			scMenuList.add(new SelectItem(CombinedSortCriterion.LAST_MODIFIED_DESC.name(), InternationalizationHelper.getLabel("sort_criterion_lastModified")));
 		}
 		
