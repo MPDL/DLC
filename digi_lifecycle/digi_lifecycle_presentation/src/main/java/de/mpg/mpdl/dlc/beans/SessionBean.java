c5a05c3e6fc8391ed4e3c9eed5e64119d56376b3
schlender <schlender@28ceb770-7456-11e2-abc6-2dfc910b5e1a>
2012-11-08 13:50:58 +0000

    GUI work - set the right logo in header on volumes.xhtml

--- digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/dlc/beans/SessionBean.java
+++ digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/dlc/beans/SessionBean.java
@@ -5,6 +5,7 @@ import gov.loc.www.zing.srw.SearchRetrieveRequestType;
 import java.io.Serializable;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -62,88 +63,102 @@ public class SessionBean implements Serializable
 		} 
 
     }
-
-	  public String getLogoUrl()
-	    {    	
-	    	
-	    	//Reset url on common pages to default dlc logo
-	    	String viewId = FacesContext.getCurrentInstance().getViewRoot().getViewId();
-	    	if (viewId.equals("/Welcome.xhtml")
-	    			|| viewId.equals("/volumes.xhtml")
-	    			|| viewId.equals("/ingest.xhtml")
-	    			|| viewId.equals("/advancedSearch.xhtml")
-	    			||viewId.equals("/admin.xhtml"))
-	    		{logoUrl = "";
-	    		setLogoTlt("DLC");} 
-	    	if (logoUrl == null || logoUrl.equals("")) 
-	    		{return "/resources/images/solution_logo_216_75.png";}
-	    	else return logoUrl;
-	    }
-	    
-	    
-	    public void setLogoUrl(String url)
+	
+	private Boolean useDLCLogo()
+	{	//Reset url on common pages to default dlc logo
+		String viewId = FacesContext.getCurrentInstance().getViewRoot().getViewId();
+    	
+    	if (viewId.equals("/Welcome.xhtml")
+    		|| viewId.equals("/ingest.xhtml")
+    		|| viewId.equals("/advancedSearch.xhtml")
+    		|| viewId.equals("/advancedSearchResult.xhtml")
+    		|| viewId.equals("/admin.xhtml"))
+    	{	
+    		return true;
+    	} 
+    	else 
+    	{
+    		return false;
+    	}
+    	
+	}
+	
+	public String getLogoUrl()
+	{    	
+		if (useDLCLogo())
+		{
+			logoUrl = "";
+			setLogoTlt("DLC");
+		}
+		
+		if (logoUrl == null || logoUrl.equals("")) 
+		{
+			return "/resources/images/solution_logo_216_75.png";
+		}
+		else return logoUrl;
+	}
+	
+	public void setLogoUrl(String url)
+	{
+		logoUrl = url;
+	}
+	
+	public String getLogoLink()
+	{    	  	
+	    if (useDLCLogo())
+		{
+	    	logoLink = "";
+		}
+	    if (logoLink == null || logoLink.equals(""))
 	    {
-	    	logoUrl = url;
+	    	return getAppBean().getDomain()+"/"+getAppBean().getContextPath()+"/";
 	    }
-	    
-	    public String getLogoLink()
-	    {    	  	
-	    	//Reset link on common pages to default dlc logo
-	    	String viewId = FacesContext.getCurrentInstance().getViewRoot().getViewId();
-	    	if (viewId.equals("/Welcome.xhtml")
-	    			|| viewId.equals("/volumes.xhtml")
-	    			|| viewId.equals("/ingest.xhtml")
-	    			|| viewId.equals("/advancedSearch.xhtml")
-	    			||viewId.equals("/admin.xhtml"))
-	    		{logoLink = "";} 
-	    	if (logoLink == null || logoLink.equals("")) 
-	    		{return getAppBean().getDomain()+"/"+getAppBean().getContextPath()+"/";}
-	    	return logoLink;
-	    }
-	    
-	    
-	    public void setLogoLink(String id)
-	    {	
-	    	String url = getAppBean().getDomain() + "/"+getAppBean().getContextPath()+"/ou/" + id;
-	    	logoLink = url;
-	    }
-	    
-	    public String getLogoTlt()
-	    {
-	    	//Reset tlt on common pages to default dlc logo
-	    	String viewId = FacesContext.getCurrentInstance().getViewRoot().getViewId();
-	    	if (viewId.equals("/Welcome.xhtml")
-	    			|| viewId.equals("/volumes.xhtml")
-	    			|| viewId.equals("/ingest.xhtml")
-	    			|| viewId.equals("/advancedSearch.xhtml")
-	    			||viewId.equals("/admin.xhtml"))
-	    		{logoTlt = "";} 
-	    	if (logoTlt == null || logoTlt.equals("")) 
-	    		{logoTlt = InternationalizationHelper.getTooltip("main_home").replace("$1", "DLC");}
-	    	return logoTlt;
-	    }
-	    
-	    public void setLogoTlt(String tlt)
-	    {
-	    	logoTlt = tlt;
-	    }
-	    
-	    
-	    public String getLatestCql() {
-			return latestCql;
+	    return logoLink;
+	}
+	
+	public void setLogoLink(String id)
+	{
+		String url = getAppBean().getDomain() + "/"+getAppBean().getContextPath()+"/ou/" + id;
+		logoLink = url;
+	}
+	
+	public String getLogoTlt()
+	{
+    	if (useDLCLogo())
+		{
+    		logoTlt = "";
 		}
-
-		public void setLatestCql(String cql) {
-			latestCql = cql;
-		}
-
-		public ApplicationBean getAppBean() {
-			return appBean;
-		}
-
-		public void setAppBean(ApplicationBean appBean) {
-			this.appBean = appBean;
-		}
+    	if (logoTlt == null || logoTlt.equals(""))
+    	{
+    		logoTlt = InternationalizationHelper.getTooltip("main_home").replace("$1", "DLC");
+    	}
+    	return logoTlt;
+	}
+	
+	public void setLogoTlt(String tlt)
+	{
+		logoTlt = tlt;
+	}
+	
+	public String getLatestCql() 
+	{
+		return latestCql;
+	}
+	
+	public void setLatestCql(String cql) 
+	{
+		latestCql = cql;
+	}
+	
+	public ApplicationBean getAppBean() 
+	{
+		return appBean;
+	}
+	
+	public void setAppBean(ApplicationBean appBean) 
+	{
+		this.appBean = appBean;
+	}
     
 }
 
