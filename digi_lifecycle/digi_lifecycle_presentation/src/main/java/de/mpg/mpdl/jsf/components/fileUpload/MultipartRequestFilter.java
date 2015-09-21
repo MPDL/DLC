545d1ab975d479e2ff174ca3c6cdfca84f7e567f
mfranke <13797288+franke17@users.noreply.github.com>
2015-09-21 14:43:46 +0000

    Licences

--- digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/jsf/components/fileUpload/MultipartRequestFilter.java
+++ digi_lifecycle/digi_lifecycle_presentation/src/main/java/de/mpg/mpdl/jsf/components/fileUpload/MultipartRequestFilter.java
@@ -8,8 +8,16 @@
  * When distributing Covered Code, include this CDDL HEADER in each file and include the License file at license/ESCIDOC.LICENSE. If applicable, add the following below this CDDL HEADER, with the fields enclosed by brackets "[]" replaced with your own identifying information: Portions Copyright [yyyy] [name of copyright owner]
  * CDDL HEADER END
  * 
- * Copyright 2006-2013 Fachinformationszentrum Karlsruhe Gesellschaft für wissenschaftlich-technische Information mbH and Max-Planck-Gesellschaft zur Förderung der Wissenschaft e.V.
+ * Copyright 2011-2015 Fachinformationszentrum Karlsruhe Gesellschaft für wissenschaftlich-technische Information mbH and Max-Planck-Gesellschaft zur Förderung der Wissenschaft e.V.
  * All rights reserved. Use is subject to license terms.
+ *
+ * Initial Developer (as defined by CDDL 1.0) of the DLC-Software are five
+ * institutes and facilities of the Max Planck Society: Bibliotheca Hertziana 
+ * (Rome), Kunsthistorisches Institut in Florenz (Florence), Max Planck Institute
+ * for European Legal History (Frankfurt a.M.), Max Planck Institute for Human
+ * Development (Berlin), and the Max Planck Digital Library (Munich). Users of the
+ * DLC-Software are requested to include a "powered by DLC" on their webpage,
+ * linking to the DLC documentation (http://dlcproject.wordpress.com/).
  ******************************************************************************/
 package de.mpg.mpdl.jsf.components.fileUpload;
 /*
---
+++ digi_lifecycle/readme.txt
@@ -0,0 +1,7 @@
+Initial Developer (as defined by CDDL 1.0) of the DLC-Software are five
+institutes and facilities of the Max Planck Society: Bibliotheca Hertziana 
+(Rome), Kunsthistorisches Institut in Florenz (Florence), Max Planck Institute
+for European Legal History (Frankfurt a.M.), Max Planck Institute for Human
+Development (Berlin), and the Max Planck Digital Library (Munich). Users of the
+DLC-Software are requested to include a "powered by DLC" on their webpage,
+linking to the DLC documentation (http://dlcproject.wordpress.com/).
